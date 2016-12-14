8f183f677619484622d6bc0cb8b8edc1258311e5
Pepe Valdivia <jvaldivia@softweb.pe>
2016-12-14 11:37:06 -0500

    Añadida función de generar claves de longitud 16

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask.ext.cors import CORS, cross_origin
 from Crypto.Cipher import AES
 import base64
 import sys 
+import random
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -16,7 +17,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def index():
 	return 'Error : URI vacía'
 
-@app.route('/encode', methods=['GET', 'POST'])
+@app.route('/encode', methods=['POST'])
 def encode():
 	key = request.args.get('key')
 	texto = request.args.get('texto')
@@ -24,13 +25,17 @@ def encode():
 	
 	return base64.b64encode(cipher.encrypt(texto.rjust(32)))
 
-@app.route('/decode', methods=['GET', 'POST'])
+@app.route('/decode', methods=['POST'])
 def decode():
 	key = request.args.get('key')
 	texto = request.args.get('texto')
 	cipher = AES.new(key,AES.MODE_ECB)
 	
 	return cipher.decrypt(base64.b64decode(texto)).strip()
+
+@app.route('/key', methods=['GET'])
+def key():	
+	return ''.join(random.choice('0123456789ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') for i in range(16))
 	
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=5000)
